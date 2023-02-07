3f7d9bbfd959eeea0deed80f823e4cd9385b4d23
Marcin <94755972+Marcinzale@users.noreply.github.com>
2023-02-07 17:48:12 +0100

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Alert } from 'react-native';
+import React, { useState } from "react";
+
+import QRCode from 'react-native-qrcode-svg';
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
+  const [text, setText] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textInput}
+        placeholder='Value to generate QR Code'
+        underlineColorAndroid="transparent"
+        value={text}
+        onChangeText={txt => {
+          if (txt == '') {
+            setText = 'NA'
+          } else {
+            setText(txt);
+          }
+        }}
+
+      />
+
+      {/* <Button title='Generate QR Code'
+        onPress={txt => {
+          if (text == '') {
+            setText = "Input Value"
+          } else {
+            setText(text)
+          }
+        }}>
+      </Button> */}
+      <View >
+        <QRCode value={text} size={300} />
+      </View>
     </View>
   );
 }
@@ -13,8 +44,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'darkgrey',
     alignItems: 'center',
     justifyContent: 'center',
+    underlineColorAndroid: 'transparent',
+  },
+
+  textInput: {
+    width: '80%',
+    backgroundColor: "white",
+    borderColor: 'grey',
+    borderStyle: 'solid',
+    borderRadius: 10,
+    borderWidth: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    color: 'blue',
+    padding: 10,
+    marginVertical: 20,
+    underlineColorAndroid: 'transparent',
+  },
+
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
